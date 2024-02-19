444a9e5bcb55871b122a07d350da7a8075236318
runner <runner@Mac-1708350355714.local>
2024-02-19 14:10:43 +0000

    KMM SPM package release for 1.3.1620240219140357

--- Package.swift
+++ Package.swift
@@ -1,23 +1,29 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.3
 import PackageDescription
 
+// BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
+let remoteKotlinUrl = "https://maven.pkg.github.com/HedvigInsurance/authlib/com/hedvig/authlib/authlib-kmmbridge/1.3.16-alpha-20240219140357/authlib-kmmbridge-1.3.16-alpha-20240219140357.zip"
+let remoteKotlinChecksum = "6f0d7c69f11919ec1eac61a3dc93b1a97cc53c17957c1c26e9382c0b39579127"
 let packageName = "authlib"
+// END KMMBRIDGE BLOCK
+
 let package = Package(
     name: packageName,
     platforms: [
-        .iOS(.v14),
+        .iOS(.v13)
     ],
     products: [
         .library(
             name: packageName,
             targets: [packageName]
-        )
+        ),
     ],
-    dependencies: [],
     targets: [
         .binaryTarget(
             name: packageName,
-            path: "./authlib/build/XCFrameworks/release/\(packageName).xcframework"
+            url: remoteKotlinUrl,
+            checksum: remoteKotlinChecksum
         )
+        ,
     ]
 )
