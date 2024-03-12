9b5830cea0c4ecf883b7b5540744397b547bb116
stylianosgakis <stylianos.gakis98@gmail.com>
2024-03-12 11:32:11 +0100

    Remove unused AuthlibError

--- src/commonMain/kotlin/com/hedvig/authlib/error/AuthlibError.kt
+++
@@ -1,8 +0,0 @@
-package com.hedvig.authlib.error
-
-public sealed interface AuthlibError {
-    public data class Localised(val reason: String) : AuthlibError
-    public data class BackendErrorResponse(val message: String, val httpStatusValue: Int) : AuthlibError
-    public data class IOError(val message: String) : AuthlibError
-    public data class UnknownError(val message: String) : AuthlibError
-}
