db502bfce0f1e6d5206ac17b38d4c8d9fcf1ae4e
stylianosgakis <stylianos.gakis98@gmail.com>
2024-10-09 10:52:11 +0200

    Remove support for legacy connect payment
    It is no longer supported by the web

--- src/commonMain/kotlin/com/hedvig/authlib/AuthEnvironment.kt
+++ src/commonMain/kotlin/com/hedvig/authlib/AuthEnvironment.kt
@@ -9,15 +9,3 @@ internal val AuthEnvironment.baseUrl: String
         AuthEnvironment.STAGING -> "https://auth.dev.hedvigit.com"
         AuthEnvironment.PRODUCTION -> "https://auth.prod.hedvigit.com"
     }
-
-internal val AuthEnvironment.gatewayUrl: String
-    get() = when (this) {
-        AuthEnvironment.STAGING -> "https://gateway.test.hedvigit.com"
-        AuthEnvironment.PRODUCTION -> "https://gateway.hedvig.com"
-    }
-
-internal val AuthEnvironment.webBaseUrl: String
-    get() = when (this) {
-        AuthEnvironment.STAGING -> "https://dev.hedvigit.com"
-        AuthEnvironment.PRODUCTION -> "https://www.hedvig.com"
-    }
--- src/commonMain/kotlin/com/hedvig/authlib/connectpayment/MemberAuthorizationCodesResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.hedvig.authlib.connectpayment
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-internal data class MemberAuthorizationCodesResponse(
-    val authorizationCode: String,
-)
